3585045afa47c2eac616206602a45282bb37201c
Joshua Kyalo <56042440+Yusha254@users.noreply.github.com>
2025-09-14 20:51:56 +0300

    Update main.ts

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,25 @@
+interface MajorCredits {
+  credits: number;
+  // brand property to distinguish from MinorCredits
+  __brand: "major";
+}
+
+interface MinorCredits {
+  credits: number;
+  // brand property to distinguish from MajorCredits
+  __brand: "minor";
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: "major",
+  };
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: "minor",
+  };
+}
